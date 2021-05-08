0fac52d5656f60216d38783e713cf06377702428
Mike Lynch <me@msl.sh>
2021-05-08 00:39:58 -0400

    Add implementation of AutoRerunTasks
    These are tasks which generate hashes to track their input and parameters,
    so that they can automatically be rerun if these change,
    without having to manually delete out of date files first.

--- src/luigi_utilities/__init__.py
+++ src/luigi_utilities/__init__.py
@@ -1,3 +1,4 @@
+from luigi_utilities.auto_rerun_task import AutoRerunTask, RerunExtTask
 from luigi_utilities.dynamic import new_task, pick_output
 from luigi_utilities.multi_target_manager import multi_target_manager
 
@@ -6,6 +7,8 @@ __version__ = "0.1.0"
 
 __all__ = [
     "__version__",
+    "AutoRerunTask",
+    "RerunExtTask",
     "multi_target_manager",
     "new_task",
     "pick_output",
