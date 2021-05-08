a026e86358654441225464756ca8cd25c38222bf
Mike Lynch <me@msl.sh>
2021-05-08 14:56:37 -0400

    Update version number

--- src/luigi_gadd/__init__.py
+++ src/luigi_gadd/__init__.py
@@ -1,8 +1,10 @@
+import pkg_resources
+
 from luigi_gadd.auto_rerun_task import AutoRerunTask, RerunExtTask
 from luigi_gadd.dynamic import new_task, pick_output
 from luigi_gadd.multi_target_manager import multi_target_manager
 
-__version__ = "0.2.0"
+__version__ = pkg_resources.get_distribution("luigi-gadd").version
 
 
 __all__ = [
